da31b44dcd10ddba54d88a6db0fe2d6cf63e6e5a
gkanawati <gabielkanawati3@gmail.com>
2021-09-20 18:45:29 -0300

    delete

--- Avaliação 2 Prog (Run Codes)/MaiorPalavra.c
+++ Avaliação 2 Prog (Run Codes)/MaiorPalavra.c
@@ -4,14 +4,14 @@
 int main()
 {
     char str[2000];
-    scanf("%[^\n]s", &str);
+    scanf("%[^\n]s", str);
 
     int word_size = 0;
     int greatest = 0;
 
     for (int i = 0; str[i] != '\0'; i++)
     {
-        while (str[i] != ' ' && str[i + 1] != '\n' && str[i] != '.')
+        while (str[i] != ' ' && str[i + 1] != '\0' && str[i] != '.')
         {
             word_size++;
             i++;
