bfc03760e74a5f14994525a1897b1213c17f3be1
Gabriel Loutfalla Kanawati Rosário <gabrielkanawati3@gmail.com>
2021-12-13 00:11:16 -0300

    strcut

--- PROG II/5.Estruturas/Aula4Ponteiros.c
+++ PROG II/5.Estruturas/Aula4Ponteiros.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 struct endereco
@@ -10,12 +11,38 @@ struct endereco
 
 int main()
 {
+   /* 
+    Modo 1 Usando Ponteiro
+    
     struct endereco *end;
     end = malloc(sizeof(struct endereco));
 
     fgets(end->nome, sizeof(end->nome), stdin);
+    strtok(end->nome, "\n");
     fgets(end-> rua, sizeof(end->rua), stdin);
+    strtok(end->rua, "\n");
     scanf("%d%*c", &end->numero);
+
+    printf("\n%s", end->nome);
+    printf("\n%s", end->rua);
+    printf("\n%d", end->numero); 
+    
+    free(end); */
+
+    struct endereco end1;
+
+    fgets(end1.nome, sizeof(end1.nome), stdin);
+    strtok(end1.nome, "\n"); // A função strtok retira o \n do final da string que o fgets lê
+    fgets(end1.rua, sizeof(end1.rua), stdin);
+    strtok(end1.rua, "\n");
+    scanf("%d%*c", &end1.numero);
+
+    struct endereco *endpoint;
+    endpoint = &end1;
+
+    printf("\n%s", endpoint->nome);
+    printf("\n%s", endpoint->rua);
+    printf("\n%d", endpoint->numero);
 
     return 0;
 }
---
+++ PROG II/5.Estruturas/Ex7.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+struct raizes
+{
+    double x1;
+    double x2;
+};
+
+int main()
+{
+
+    struct raizes r1 = {1.0, 2.0};
+    struct raizes r2 = {1.0, 2.0};
+
+    if (r1 == r2)
+    {
+        printf("sim");
+    }
+    else
+    {
+        printf("nao");
+    }
+
+    return 0;
+}
