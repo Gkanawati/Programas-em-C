4a47264560d733484bb1650ed4675ee5bc8b48ea
Gkanawati <gabrielkanawati3@gmail.com>
2022-06-02 16:17:53 -0300

    Listas

---
+++ 3.ESTRUTURA DE DADOS/6 - Lista/4.ListaDuplamenteEncadeada&Ordenada.c
@@ -0,0 +1,176 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int dado;
+    struct node *prox;
+    struct node *ant;
+};
+
+struct Lista
+{
+    struct node *inicio;
+    struct node *fim;
+} Lista;
+
+void inicializar(struct Lista *lista);
+int inserir(struct Lista *lista, int dado);
+int inserirFim(struct Lista *lista, int dado);
+int mostrar(struct Lista lista);
+int remover(struct Lista *lista, int dado);
+int menu();
+
+int main()
+{
+
+    struct Lista lista;
+
+    inicializar(&lista);
+
+    inserir(&lista, 3);
+    inserir(&lista, 6);
+    inserir(&lista, 5);
+    inserir(&lista, 1);
+
+    mostrar(lista);
+
+    remover(&lista, 5);
+    remover(&lista, 6);
+    remover(&lista, 1);
+
+    mostrar(lista);
+
+    return 0;
+}
+
+void inicializar(struct Lista *lista)
+{
+    lista->inicio = NULL;
+    lista->fim = NULL;
+}
+
+int inserir(struct Lista *lista, int dado)
+{
+    struct node *novo = malloc(sizeof(struct node));
+
+    if (novo == NULL)
+        return 0;
+
+    novo->dado = dado;
+
+    if (lista->inicio == NULL)
+    {
+        novo->prox = NULL;
+        novo->ant = NULL;
+        lista->inicio = novo;
+        lista->fim = novo;
+    }
+    else
+    {
+        struct node *aux = NULL;
+        aux = lista->inicio;
+        while (aux != NULL && novo->dado > aux->dado)
+        {
+            aux = aux->prox;
+        }
+
+        if (aux == lista->inicio)
+        {
+            novo->prox = lista->inicio;
+            novo->ant = NULL;
+            lista->inicio->ant = novo;
+            lista->inicio = novo;
+        }
+        else if (aux == NULL)
+        {
+            lista->fim->prox = novo;
+            novo->ant = lista->fim;
+            lista->fim = novo;
+            lista->fim->prox = NULL;
+        }
+        else
+        {
+            novo->prox = aux;
+            aux->ant->prox = novo;
+            novo->ant = aux->ant;
+            aux->ant = novo;
+        }
+    }
+
+    return 1;
+}
+
+int remover(struct Lista *lista, int dado)
+{
+    struct node *aux;
+
+    int qtdDel = 0;
+    aux = lista->inicio;
+
+    if (aux == NULL)
+    {
+        return 0;
+    }
+
+    while (aux != NULL)
+    {
+        if (aux->dado == dado)
+        {
+            qtdDel++;
+            if (aux == lista->inicio)
+            {
+                lista->inicio = aux->prox;
+                if (lista->inicio != NULL)
+                {
+                    lista->inicio->ant = NULL;
+                }
+                free(aux);
+                aux = lista->inicio;
+            }
+            else if (aux == lista->fim)
+            {
+                lista->fim = lista->fim->ant;
+                lista->fim->prox = NULL;
+                free(aux);
+                aux = NULL;
+            }
+            else
+            {
+                aux->ant->prox = aux->prox;
+                aux->prox->ant = aux->ant;
+                struct node *aux2;
+                aux2 = aux->prox;
+                free(aux);
+                aux = aux2;
+            }
+        }
+        else
+        {
+            aux = aux->prox;
+        }
+    }
+
+    return qtdDel;
+}
+
+int mostrar(struct Lista lista)
+{
+    struct node *aux;
+
+    if (lista.inicio == NULL)
+    {
+        return 0;
+    }
+    else
+    {
+        aux = lista.inicio;
+        while (aux != NULL)
+        {
+            printf("%d ", aux->dado);
+            aux = aux->prox;
+        }
+    }
+    printf("\n");
+    return 1;
+}
---
+++ 3.ESTRUTURA DE DADOS/6 - Lista/5.ListaCircular.c
@@ -0,0 +1,164 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int dado;
+    struct node *prox;
+};
+
+struct Lista
+{
+    struct node *inicio;
+    struct node *fim;
+} Lista;
+
+void inicializar(struct Lista *lista);
+int inserirInicio(struct Lista *lista, int dado);
+int inserirFim(struct Lista *lista, int dado);
+int mostrar(struct Lista lista);
+int remover(struct Lista *lista, int dado);
+int menu();
+
+int main()
+{
+
+    struct Lista lista;
+    inicializar(&lista);
+
+    inserirInicio(&lista, 1);
+    inserirInicio(&lista, 2);
+    inserirInicio(&lista, 3);
+    inserirFim(&lista, 4);
+    inserirFim(&lista, 5);
+    mostrar(lista);
+    remover(&lista, 3);
+    mostrar(lista);
+    remover(&lista, 1);
+    mostrar(lista);
+    remover(&lista, 5);
+    mostrar(lista);
+
+    return 0;
+}
+
+void inicializar(struct Lista *lista)
+{
+    lista->inicio = NULL;
+    lista->fim = NULL;
+}
+
+int inserirInicio(struct Lista *lista, int dado)
+{
+    struct node *novo = malloc(sizeof(struct node));
+
+    if (novo == NULL)
+        return 0;
+
+    novo->dado = dado;
+
+    if (lista->inicio == NULL)
+    {
+        lista->inicio = novo;
+        lista->fim = novo;
+        (lista->fim)->prox = lista->inicio;
+    }
+    else
+    {
+        novo->prox = lista->inicio;
+        lista->inicio = novo;
+        lista->fim->prox = lista->inicio;
+    }
+
+    return 1;
+}
+
+int inserirFim(struct Lista *lista, int dado)
+{
+    struct node *novo = malloc(sizeof(struct node));
+    novo->dado = dado;
+
+    if (novo == NULL)
+        return 0;
+
+    if (lista->inicio == NULL)
+    {
+        lista->inicio = novo;
+        lista->fim = novo;
+        lista->fim->prox = lista->inicio;
+    }
+    else
+    {
+        lista->fim->prox = novo;
+        lista->fim = novo;
+        lista->fim->prox = lista->inicio;
+    }
+
+    return 1;
+}
+
+int remover(struct Lista *lista, int dado)
+{
+    struct node *aux;
+    struct node *ant;
+    int qtdDel = 0;
+
+    aux = lista->inicio;
+    ant = NULL;
+
+    do
+    {
+        if (aux->dado == dado)
+        {
+            qtdDel++;
+            if (aux == lista->inicio)
+            {
+                lista->inicio = aux->prox;
+                lista->fim->prox = lista->inicio;
+                free(aux);
+                aux = lista->inicio;
+            }
+            else if (aux == lista->fim)
+            {
+                lista->fim = ant;
+                lista->fim->prox = lista->inicio;
+                free(aux);
+                aux = lista->inicio;
+            }
+            else
+            {
+                ant->prox = aux->prox;
+                free(aux);
+                aux = ant->prox;
+            }
+        }
+        else
+        {
+            ant = aux;
+            aux = aux->prox;
+        }
+    } while (aux != lista->inicio);
+
+    return qtdDel;
+}
+
+int mostrar(struct Lista lista)
+{
+    struct node *aux;
+
+    if (lista.inicio == NULL)
+    {
+        return 0;
+    }
+    else
+    {
+        aux = lista.inicio;
+        do
+        {
+            printf("%d ", aux->dado);
+            aux = aux->prox;
+        } while (aux != lista.inicio);
+    }
+    printf("\n");
+    return 1;
+}
